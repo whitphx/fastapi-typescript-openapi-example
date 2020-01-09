22d760d963394f340622ea402fdcf72f02cd240f
Yuichiro Tsuchiya <t.yic.yt@gmail.com>
2020-01-09 17:26:57 +0900

    Add mysql container

--- backend/app/app/database.py
+++ backend/app/app/database.py
@@ -6,7 +6,7 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
